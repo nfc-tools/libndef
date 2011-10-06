4bb13f723368159bf193ef8dcae8f02acf557dde
Romuald Conty <romuald@libnfc.org>
2011-10-06 22:29:11 +0000

    libndef/ndefrecord.cpp: new a static method to extract Text field from TextRecord.
        static QString textText(const QByteArray& payload);

--- include/ndef/ndefrecord.h
+++ include/ndef/ndefrecord.h
@@ -102,6 +102,7 @@ public:
 public:
     static NDEFRecord createTextRecord(const QString& text, const QString& locale, NDEFRecordTextCodec codec = NDEFRecord::NDEF_UTF8);
     static QByteArray textLocale(const QByteArray& payload);
+    static QString textText(const QByteArray& payload);
 
     // URI records.
 public:
